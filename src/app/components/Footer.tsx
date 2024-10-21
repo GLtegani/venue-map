045120990ebbe5f0309a06e9e8d7924f41304d41
Gabriel Tegani <gabrielbtegani@gmail.com>
2024-10-21 20:37:18 -0300

    feat: add footer to main page

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { MapPin } from "@phosphor-icons/react/dist/ssr";
 export default function Footer() {
   return (
     <footer className="bg-white text-left pb-4">
-      <div className="mx-3 lg:mx-20 xl:mx-40 flex flex-col gap-2 2.5sm:gap-4 2.5sm:flex-row 2.5sm:items-center 2.5sm:justify-between">
+      <div className="mx-3 lg:mx-20 flex flex-col gap-2 2.5sm:gap-4 2.5sm:flex-row 2.5sm:items-end 2.5sm:justify-between">
         <div className="flex flex-col justify-center items-start gap-2">
           <div>
             <Logo textColor="black" withBg={false} />
@@ -49,13 +49,13 @@ export default function Footer() {
           </nav>
 
           <div
-            className="mt-1 hidden 2.5sm:flex 2md:flex-row 2md:items-center 2md:gap-5 2.5sm:mt-0 
-          2.5sm:text-left 2.5sm:flex-col 2.5sm:items-start 2.5sm:justify-center 2.5sm:text-lg"
+            className="mt-1 hidden 2.5sm:flex 2.5sm:mt-0 
+          2.5sm:text-left 2.5sm:flex-col 2.5sm:items-start 2.5sm:justify-center 2.5sm:text-lg xl:text-xl"
           >
             <p className="flex items-center justify-center gap-1 flex-nowrap">
               {<MapPin />} R. Narciso Sturlini, 62 - Centro
             </p>
-            <p className="text-nowrap">
+            <p className="text-nowrap 2md:hidden">
               &copy; Desenvolvido por Gabriel Tegani
             </p>
           </div>
@@ -69,6 +69,10 @@ export default function Footer() {
           </p>
           <p className="text-nowrap">&copy; Desenvolvido por Gabriel Tegani</p>
         </div>
+
+        <p className="text-nowrap hidden 2md:block text-lg xl:text-xl">
+          &copy; Desenvolvido por Gabriel Tegani
+        </p>
 
         <nav className="2.5sm:block 2.5sm:text-right hidden">
           <ul>
