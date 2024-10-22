4158c2d29253312670fc19a59e354bd42fdb907c
Recepcao <gabrielbtegani@gmail.com>
2024-10-22 13:49:29 -0300

    build: add shadcdn dependency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,32 +9,73 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      screens: {
-        "3sm": "320px",
-        "2.5sm": "465px",
-        "2sm": "490px",
-        "1sm": "590px",
-        "2md": "876px",
-        "1md": "740px",
-        ls: "540px",
-      },
-      backgroundImage: {
-        chronos:
-          "linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.9)), url(/chronos.png)",
-        chronosPonte:
-          "linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.9)), url(/ponte-metalica-chronos.png)",
-      },
-      width: {
-        "11.5/12": "95%",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		screens: {
+  			'3sm': '320px',
+  			'2.5sm': '465px',
+  			'2sm': '490px',
+  			'1sm': '590px',
+  			'2md': '876px',
+  			'1md': '740px',
+  			ls: '540px'
+  		},
+  		backgroundImage: {
+  			chronos: 'linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.9)), url(/chronos.png)',
+  			chronosPonte: 'linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.9)), url(/ponte-metalica-chronos.png)'
+  		},
+  		width: {
+  			'11.5/12': '95%'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  darkMode: ["class", "class"],
+  plugins: [nextui(), require("tailwindcss-animate")],
 };
 export default config;
